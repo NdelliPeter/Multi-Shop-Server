ff2ed03e96539422013b2b631d6a8e303f90ec87
BlueSky Studios <ndellipetex@gmail.com>
2023-07-25 06:12:35 +0100

    update

--- controllers/account.controller.js
+++ controllers/account.controller.js
@@ -9,14 +9,6 @@ const Account = db.accounts
 
 const bcrypt = require('bcryptjs')
 
-const accountDataPath = './data/account.json'
-
-// const saveAccountData = (array) => {
-//     const finalArray = JSON.stringify(array)
-//     fs.writeFileSync(accountDataPath, finalArray)
-// }
-
-
 const getAllAccounts = (req, res) => {
     pool.query(`Select * from accounts`, (err, result) => {
         if (!err) {
@@ -33,10 +25,7 @@ const getAllAccounts = (req, res) => {
 const createAccount = async (req, res) => {
 
     const {id, fullname, userName, email, password} = req.body
-
-
     try {
-
         const data = {
             id: id,
             fullname: fullname,
@@ -44,9 +33,7 @@ const createAccount = async (req, res) => {
             email: email,
             password: await bcrypt.hash(password, 10)
         }
-
         console.log(data);
-
         // Saving an account
         const account = await Account.create(data)
         if (account) {
@@ -124,88 +111,3 @@ module.exports = {
     getAccount
 }
 
-
-// const bcrypt = require('bcrypt');
-// const db = require('../models');
-// const jwt = require('jsonwebtoken');
-
-// const Account = db.accounts;
-
-
-// const signup = async (req, res) => {
-//         const { id, firstname, lastname, username, email, password} = req.body;
-
-//     try {
-//         const data = {
-//             id,
-//             firstname,
-//             lastname,
-//             username,
-//             email,
-//             password: await bcrypt.hash(password, 10),
-
-//         }
-
-//         const account = await Account.create(data);
-
-
-//         if ( account ) {
-//             let token = jwt.sign({id: account.id}, process.env.secretKey, {
-//                 expiresIn: 1 * 24 * 60 * 1000,
-//             });
-
-//             res.cookie('jwt', token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true});
-//             console.log('account', JSON.stringify(account, null, 2));
-//             console.log(token);
-//             return res.status(201).send(account)
-//         }else {
-//             return res.status(409).send('Details are not cottect');
-//         }
-
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-// const login =async (req, res) => {
-//     try {
-//         const { email, password } = req.body;
-
-//         const account = await Account.findOne({
-//             where: {
-//                 email: email
-//             }
-//         });
-
-
-//         if (account) {
-//             const isSame = await bcrypt.compare(password, account.password);
-
-
-
-//             if (isSame) {
-//                 let token = jwt.sign({id: account.id}, process.env.secretKey, {
-//                     expiresIn: 1 * 24 * 60 * 1000,
-//                 });
-
-//                 res.cookie('jwt', token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true});
-//             console.log('account', JSON.stringify(account, null, 2));
-//             console.log(token);
-//             return res.status(201).send(account)
-            
-//             }else {
-//                 return res.status(401).send("Authentication failed");
-//             }
-//         }else {
-//             return res.status(401).send("Authentication failed");
-//         }
-//     } catch (error) {
-//         console.log(error);
-//     }
-// };
-
-
-// module.exports = {
-//     signup,
-//     login
-// }
